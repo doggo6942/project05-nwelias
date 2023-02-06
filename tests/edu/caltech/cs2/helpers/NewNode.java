1136a4b37fe19919a8ea013a0ae4d616c54aa2c1
blank <blank@caltech.edu>
2023-02-05 21:56:19 -0800

    Add to NewNode

--- tests/edu/caltech/cs2/helpers/NewNode.java
+++ tests/edu/caltech/cs2/helpers/NewNode.java
@@ -4,4 +4,5 @@ public class NewNode {
     public static int LinkedDeque_NUM_CALLS;
     public static int MoveToFrontDictionary_NUM_CALLS;
     public static int BSTDictionary_NUM_CALLS;
+    public static int TrieMap_NUM_CALLS;
 }
